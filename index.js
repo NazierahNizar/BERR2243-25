8b50e381f7a8be49c4fdd24879a59616214a7c3f
Nazierah Nizar <nazierahnizarr@gmail.com>
2025-04-23 14:09:54 +0800

    Test commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const express = require('express');
 const { MongoClient } = require('mongodb');
+const port = 3000
 
-async function main() {
+const app = express();
+app.use(express.json());
+
+let db;
+
+async function connectToMongoDB() {
     const uri = "mongodb://localhost:27017";
     const client = new MongoClient(uri);
 
@@ -8,19 +15,67 @@ async function main() {
         await client.connect();
         console.log("Connected to MongoDB!");
 
-        const db = client.db("testDB");
-        const collection = db.collection("users");
-
-        await collection.insertOne({ name: "Zaeem" , age: 60 });
-        console.log("Document inserted!");
-
-        const result = await collection.findOne({ name: "Zaeem" });
-        console.log("Query result:", result);
-    } catch (err) {
+        db = client.db("testDB");
+    }   catch (err) {
         console.error("Error:", err);
-    } finally {
-        await client.close();
     }
 }
+connectToMongoDB();
 
-main();
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+// Get /rides - Fetch all rides
+app.get('/rides' , async (req, res) => {
+    try{
+        const rides = await db.collection('rides').find().toArray();
+        res.status(200).json(rides);
+    }   catch (err) {
+        res.status(500),json({ error: "Failed to fetch rides" });
+    }
+});
+
+// POST /rides - create a new ride
+app.post('/rides' , async (req, res) => {
+    try {
+        const result = await db.collection('rides').insertOne(req,body);
+        res.status(201).json({ id: result.insertedID });
+    }   catch (err) {
+        res.status(400).json({ error: "Invalid ride data" });
+    }
+});
+
+// PATCH /rides/:id - Update ride status
+app.patch('/rides/:id', async (req, res) => {
+    try {
+        const result = await db.collection('rides').updateOne(
+            { _id: new ObjectId(req.params.id) },
+            { $set: { status: req.body.status } }
+        );
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ error: "Ride not found" });
+        }
+        res.status(200).json({  updated: result.modifiedCount });
+    }   catch (err) {
+        // Handle invalid ID format or DB errors
+        res.status(400).json({ error: "Invalid ride ID or data" });
+    }
+});
+
+//DELETE /rides/:id -Cancel a ride
+app.delete('/rides/:id', async (req, res) => {
+    try {
+        const result = await db.collection('rides').deleteOne(
+            { _id: new ObjectId(req.params.id) }
+        );
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: "Ride not found" });
+        }
+        res.status(200).json({  deleted: result.deletedCount });
+    }   catch (err) {
+        res.status(400).json({ error: "Invalid ride ID" });
+    }
+});
